568cc8485a07699b3d482fea7312c9ee3fa7bc6b
selsebiiil <selsebilcatic@gmail.com>
2024-11-27 16:58:22 +0100

    swagger

--- src/main.ts
+++ src/main.ts
@@ -4,29 +4,16 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // swagger setup
+  // Swagger setup
   const config = new DocumentBuilder()
-    .setTitle('Outecho Api Docs')
-    .setDescription('Documentation API Test')
+    .setTitle('API Docs')
+    .setDescription('The API description')
     .setVersion('1.0')
-    .setBasePath('api/v1')
-    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
+    .addTag('Outecho')
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document, {
-    customSiteTitle: 'Outecho Api Docs',
-    customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
-    customJs: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
-    ],
-    customCssUrl: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
-    ],
-  });
+  SwaggerModule.setup('api', app, document); // Swagger UI will be at '/api'
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
