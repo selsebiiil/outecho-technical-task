26845a8d9f3eb21b8b4eef3b1dd84ed0dbb0aafa
selsebiiil <selsebilcatic@gmail.com>
2024-11-27 16:48:06 +0100

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('cats') // Optional: Adds a tag to your API
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // The 'api' route will show the Swagger UI
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
